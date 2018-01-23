9345800b1f7dd9a3f6fa6d751e07f7e899431513
Beyar <Beyar-123@hotmail.com>
2018-01-23 22:25:25 +0100

    Add files via upload

---
+++ HIBP.rb
@@ -0,0 +1,43 @@
+# # # # # # # #
+#!/usr/bin/env ruby
+# # # # # # #
+
+# # # # # #
+# Created November 2th 2017
+# Copyright (c) 2017 Beyar N.
+# # # #
+
+# # #
+# Name: hibp.rb
+# #
+
+system "clear" or system "cls"
+
+begin
+	require "open-uri"
+	rescue LoadError
+	system "gem install openurl"
+end
+
+begin
+	require "nokogiri"
+	rescue LoadError
+	system "gem install nokogiri"
+end
+
+white = "\033[0m"
+cyan = "\033[0;36m"
+purple = "\033[0;35m"
+red = "\033[0;31m"
+
+@doc = Nokogiri::HTML(open("https://haveibeenpwned.com/"))
+puts "	       #{white}Haveibeenpwned's Top 10"
+puts
+puts "	           #{purple}Website | Entries"
+for x in 0..10
+	begin
+		puts "#{cyan}#{@doc.css("i.pwnCompanyName")[x].text.center(26)} #{white}| #{red}#{@doc.css("span.pwnCount")[x].text}#{white}"
+		rescue NoMethodError
+		#do noting
+	end
+end
